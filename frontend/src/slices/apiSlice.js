2dc258bc0510d0716a518c8f5d1c9de3c7a53c45
Roopesh Gowda <gowdaroopesh828@gmail.com>
2025-12-26 20:38:55 +0530

    Fix apiSlice to remove localhost baseUrl

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,26 +1,9 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
-import { BASE_URL } from '../constants';
-import { logout } from './authSlice';
-
-
-
-const baseQuery = fetchBaseQuery({
-  baseUrl: BASE_URL,
-});
-
-
-const baseQueryWithAuth = async (args, api, extraOptions) => {
-  const result = await baseQuery(args, api, extraOptions);
-
-  if (result?.error?.status === 401) {
-    api.dispatch(logout());
-  }
-
-  return result;
-};
 
 export const apiSlice = createApi({
-  baseQuery: baseQueryWithAuth,
+  baseQuery: fetchBaseQuery({
+    baseUrl: '',
+  }),
   tagTypes: ['Product', 'Order', 'User'],
   endpoints: () => ({}),
 });
