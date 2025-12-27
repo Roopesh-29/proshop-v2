a1b99a070678df8755f6649b1faf64b6ff6c02f3
Roopesh Gowda <gowdaroopesh828@gmail.com>
2025-12-27 17:20:15 +0530

    Rebuild frontend with correct BASE_URL

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,7 @@
-export const BASE_URL = ''; // SAME DOMAIN
+export const BASE_URL = ''; // SAME DOMAIN (Render)
 
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
 export const PAYPAL_URL = '/api/config/paypal';
+export const UPLOAD_URL = '/api/upload';
